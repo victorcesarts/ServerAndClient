239c63cb7fcd631eb96172735ebc6b5a3d74cd9b
victorcesarts <victor@VICTORPC.localdomain>
2022-05-25 23:56:36 -0300

    some things done

--- server.c
+++ server.c
@@ -39,7 +39,20 @@ void addSensor(int equipmentId, int sensorId, equipment* eq){
     }
 }
 
-int extractNumber(char* buff){
+bool existSensor(equipment* eq, int snsr, int numEq, int sensor){
+      
+        for (int j = 0; j < 4; j++){
+            if(eq[numEq].sensors[j] == sensor){
+                
+                printf("Sensor %d already exist in %d\n", sensor, eq[numEq].equipmentId);
+                return true;
+            }
+        }    
+ return false;
+}
+
+equipment *extractNumber(char *buff, equipment *eq){
+
     const char str[] = "in";
     char *ptr;
     int snsr = 0;
@@ -47,17 +60,18 @@ int extractNumber(char* buff){
     int sensors[3];
     int machines[4];
     bool evaluateSns = false;
-    bool evaluateMchn = false;  
-    
+    bool evaluateMchn = false;
+
     while (*buff){
         ptr = strstr(buff, str);
         if (ptr != NULL && snsr != 4){
             if (isdigit(*buff)){
                 long val = strtol(buff, &buff, 10);
-                sensors[snsr] = (int)val;
-                printf("Sensors: %d\n", sensors[snsr]);
-                snsr++;
-                evaluateSns = true;
+                if ((int)val > 0 && (int)val < 5){
+                    sensors[snsr] = (int)val;
+                    snsr++;
+                    evaluateSns = true;
+                }
             }
             else{
                 buff++;
@@ -66,23 +80,20 @@ int extractNumber(char* buff){
         else{
             if (ptr != NULL && snsr == 4){
                 printf("You must add until 3 sensors in one command line.\n");
-                return -1;
+                exit(EXIT_FAILURE);
             }
             else{
                 if (ptr == NULL){
                     if (isdigit(*buff) && machn <= 3 && evaluateSns){
                         long val = strtol(buff, &buff, 10);
-                        machines[machn] = (int)val;
-                        printf("Machines: %d\n", machines[machn]);
-                        machn++;
-                        evaluateMchn = true;
+                        if ((int)val > 0 && (int)val < 5){
+                            machines[machn] = (int)val;
+                            machn++;
+                            evaluateMchn = true;
+                        }
                     }
                     else{
                         buff++;
-                    }
-
-                    if (evaluateMchn){
-                        printf("Is that an end?\n");
                     }
                 }
             }
@@ -91,18 +102,69 @@ int extractNumber(char* buff){
 
     if (evaluateSns && !evaluateMchn){
         printf("Sintax error\n");
-        return -1;
+        exit(EXIT_FAILURE);
     }
     if (!evaluateMchn){
         printf("Sintax error\n");
-        return -1;
+        exit(EXIT_FAILURE);
     }
 
-    return *sensors;
+    for (int numEq = 0; numEq < 4; numEq++){
+        int numMachnChange = 0;
+        while (numMachnChange < machn){
+            if (eq[numEq].equipmentId == machines[numMachnChange]){
+                for (int h = 0; h < snsr; h++){
+                    for (int j = 0; j < 4; j++){
+                        bool exist = existSensor(eq, snsr, numEq, sensors[h]);
+                        if ((eq[numEq].sensors[j] == 0) && !exist){
+                            eq[numEq].sensors[j] = sensors[h];
+                            printf("Sensor %d added\n", sensors[h]);
+                            break;
+                        }
+                        else if (exist){
+                            break;
+                        }
+                    }
+                }
+            }
+            numMachnChange++;
+        }
+    }
+    return eq;
+}
+
+void listSensors(char *buff, equipment* eq){
+    const char str[] = "in";
+    char *ptr;
+     while (*buff){
+        ptr = strstr(buff, str);
+        if (ptr != NULL){
+            if (isdigit(*buff)){
+                long val = strtol(buff, &buff, 10);
+                for (int i = 0; i < 4; i++){
+                    if (eq[i].equipmentId == (int)val){
+                        for(int j = 0; j < 4; j++){
+                            if(eq[i].equipmentId != 0){
+                                printf("%d", eq[i].sensors[j]);
+                            }else{
+                                printf("none");
+                            }
+                            printf("\n");
+                        }
+                    }
+                    
+                }
+                
+            }
+        }
+        buff++;
+    } 
+
 }
 
 
 int main(int argc, char **argv) {
+     
 
     if (argc < 3) {
         usage(argc, argv);
@@ -133,42 +195,32 @@ int main(int argc, char **argv) {
         logexit("listen");
     }
 
-    char addrstr[BUFSZ];
-    addrtostr(addr, addrstr, BUFSZ);
-    printf("bound to %s, waiting connections\n", addrstr);
-
-    equipment esteira;
-    esteira.equipmentId = 01;
+    equipment esteira = {01, {0,0,0,0}};
+   
+    equipment guindaste = {02, {0,0,0,0}};
     
-    equipment guindaste;
-    guindaste.equipmentId = 02;
-
-    equipment ponteRolante;
-    ponteRolante.equipmentId = 03;
-
-    equipment empilhadeira;
-    empilhadeira.equipmentId = 04;
+    equipment ponteRolante = {03, {0,0,0,0}};
+   
+    equipment empilhadeira = {04, {0,0,0,0}};
 
     equipment equipments[4];
 
     equipments[0] = esteira;
     equipments[1] = guindaste;
     equipments[2] = ponteRolante;
-    equipments[3] = empilhadeira;
-   for (int i = 0; i <= 3; i++)
-   {
-        equipments[i].sensors[i] = 0;
-        //printf("%d\n", equipments[i].equipmentId);
-        /* printf("%d\n", equipments[i].sensors[i]); */
-   }
+    equipments[3] = empilhadeira;  
 
-    while (1) {
+    char addrstr[BUFSZ];
+    addrtostr(addr, addrstr, BUFSZ);
+    printf("bound to %s, waiting connections\n", addrstr);
+
+    while (1){
         struct sockaddr_storage cstorage;
         struct sockaddr *caddr = (struct sockaddr *)(&cstorage);
         socklen_t caddrlen = sizeof(cstorage);
 
         int csock = accept(s, caddr, &caddrlen);
-        if (csock == -1) {
+        if (csock == -1){
             logexit("accept");
         }
 
@@ -182,32 +234,45 @@ int main(int argc, char **argv) {
 
         if (((int)count) == 0){
             printf("Received %d bytes. \n", (int)count);
-        }else{
-            
+        }
+        else{
+
             regex_t regex1;
             int verifyRegx1 = regcomp(&regex1, "add sensor", REG_EXTENDED);
-
+            
             if (verifyRegx1 != 0){
                 logexit("Regex error compilation. \n");
             }
             else{
                 int matchRegx = regexec(&regex1, buf, 0, NULL, 0);
                 if (matchRegx == 0){
-                    extractNumber(buf);
-                    addSensor(5, 2, equipments);
+                    extractNumber(buf, equipments);
                     printf("[msg] %s, %d bytes: %s\n", caddrstr, (int)count, buf);
+                }else{
+                    regex_t regex2;
+                    int verifyRegx2 = regcomp(&regex2, "list sensors", REG_EXTENDED);
+
+                    if (verifyRegx2 != 0){
+                        logexit("Regex error compilation. \n");
+                    }
+                    else{
+                        int matchRegx2 = regexec(&regex2, buf, 0, NULL, 0);
+                        if (matchRegx2 == 0){
+                            listSensors(buf, equipments);
+                            printf("[msg] %s, %d bytes: %s\n", caddrstr, (int)count, buf);
+                        }
+                    }
                 }
             }
-        }
 
-        
-        sprintf(buf, "remote endpoint: %.1000s\n", caddrstr);
-        count = send(csock, buf, strlen(buf) + 1, 0);
-        if (count != strlen(buf) + 1) {
-            logexit("send");
+            sprintf(buf, "remote endpoint: %.1000s\n", caddrstr);
+            count = send(csock, buf, strlen(buf) + 1, 0);
+            if (count != strlen(buf) + 1){
+                logexit("send");
+            }
+            close(csock);
         }
-        close(csock);
     }
-
-    exit(EXIT_SUCCESS);
+        exit(EXIT_SUCCESS);
+    
 }
